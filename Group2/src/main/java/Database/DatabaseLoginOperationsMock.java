133bcbf0ac10a2d5d04598780d14fbb19ee3f2a2
Vamsi Krishna Utla <vm271757@dal.ca>
2021-02-27 20:17:34 -0400

    Registration Phase 2

--- Group2/src/main/java/Database/DatabaseLoginOperationsMock.java
+++ Group2/src/main/java/Database/DatabaseLoginOperationsMock.java
@@ -2,8 +2,8 @@ package Database;
 
 public class DatabaseLoginOperationsMock
 {
-    public boolean registerUserDatabase(String employeeID, String firstName, String lastName, String email, String user_password, String user_type, IDatabaseConnection connection)
-    {
-        return false;
-    }
+   // public boolean registerUserDatabase(String employeeID, String firstName, String lastName, String email, String user_password, String user_type, IDatabaseConnection connection)
+    //{
+      //  return false;
+    //}
 }
