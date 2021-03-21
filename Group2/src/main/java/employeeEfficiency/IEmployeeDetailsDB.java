81325bdd81cc8cd5f5e6ad428b6894e3f82cbad1
janvi <jn410076@dal.ca>
2021-03-21 13:41:01 -0300

    Interface for Database access for Employee efficiency report generation

---
+++ Group2/src/main/java/employeeEfficiency/IEmployeeDetailsDB.java
@@ -0,0 +1,11 @@
+package employeeEfficiency;
+
+import java.text.ParseException;
+
+public interface IEmployeeDetailsDB {
+
+	public boolean ticketCounts();
+
+	public boolean employee_Efficiency() throws ParseException;
+
+}
