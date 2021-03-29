27f023a31b151ffdfeefa422de3679aab089b177
janvi <jn410076@dal.ca>
2021-03-28 23:10:14 -0300

    Changes in main file

--- Group2/src/main/java/main/App.java
+++ Group2/src/main/java/main/App.java
@@ -2,26 +2,23 @@ package main;
 
 import java.text.ParseException;
 
-import Tickets.IInsertTicket;
-import Tickets.InsertTicket;
 import employeeEfficiency.IInputEmployeeDetails;
 import employeeEfficiency.InputEmployeeDetails;
-import employeeEfficiency.EmployeeDetailsDB;
-import employeeEfficiency.IEmployeeDetailsDB;
+import employeeEfficiency.EmployeePerformanceDB;
+import employeeEfficiency.IEmployeePerformanceDB;
 
 public class App 
 {
     public static void main( String[] args ) throws ParseException
     {
     	IInputEmployeeDetails employeeDetails = new InputEmployeeDetails();
-    	IInsertTicket insertTickets = new InsertTicket();
     	
-    	insertTickets.userInputTicketDetails();
     	employeeDetails.setDate("2020-01-01");
     	employeeDetails.setEmployeeId("EMP123");
     	
-    	IEmployeeDetailsDB employeedetailsDB = new EmployeeDetailsDB(employeeDetails);
-    	employeedetailsDB.ticketCounts();
-    	employeedetailsDB.employee_Efficiency();
+    	IEmployeePerformanceDB employeedetailsDB = new EmployeePerformanceDB(employeeDetails);
+    	employeedetailsDB.getticketCountsDB(); 
+    	employeedetailsDB.getemployeeEfficiencyDB();
+    	employeedetailsDB.getemployeeProductivityDB();
     } 
 }
