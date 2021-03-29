5e634a06cea85bcc22a4b09ef1472bc1bba49b3f
janvi <jn410076@dal.ca>
2021-03-28 23:06:54 -0300

    Employee efficiency calculator business logic

--- Group2/src/main/java/employeeEfficiency/EmployeeEfficiencyCalculator.java
+++ Group2/src/main/java/employeeEfficiency/EmployeeEfficiencyCalculator.java
@@ -5,40 +5,105 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
-public class EmployeeEfficiencyCalculation 
+public class EmployeeEfficiencyCalculator 
 {
-	public boolean employee_Efficiency(ResultSet resultset) throws SQLException, ParseException
+	ResultSet resultSetEfficiency = null;
+	ResultSet resultSetProductivity = null;
+
+	public EmployeeEfficiencyCalculator(ResultSet resultSet) 
 	{
-		List<Long> standarHours = new ArrayList<Long>();
-		List<Long> workingHours = new ArrayList<Long>();
-
-		while(resultset.next()) 
-    	{
-    		System.out.println("\nStart Date:" +resultset.getString("startDate"));
-    		System.out.println("\nEnd Date:" +resultset.getString("endDate"));
-    		System.out.println("\nClose Date:" +resultset.getString("closeDate"));
-    		
-    	    LocalDate startDate = LocalDate.parse(resultset.getString("startDate"));
-    	    LocalDate endDate = LocalDate.parse(resultset.getString("endDate"));
-    	    long durationGiven = ChronoUnit.DAYS.between(startDate,endDate);
-    	    standarHours.add(durationGiven);
-    	    System.out.print("\nDifference duration Given:" +durationGiven);
-
-    	    LocalDate closeDate = LocalDate.parse(resultset.getString("closeDate"));
-    	    long durationTaken = ChronoUnit.DAYS.between(startDate,closeDate);
-    	    workingHours.add(durationTaken);
-    	    System.out.print("\nDifference duration taken:" +durationTaken);
- 
-    	    if(durationTaken!=durationGiven) 
-    	    {
-    	    	System.out.print("\nEfficiency:" +((durationTaken*100)/durationGiven)); 
-    	    }
-    	}
-		return true;
-	
+		this.resultSetEfficiency = resultSet;
+		this.resultSetProductivity = resultSet;
 	}
+
+	public HashMap<Integer,Integer> calculateEmployeeEfficiency() throws SQLException, ParseException
+	{
+		WorkWeights myVar = null; 
+		int efficiency = 0;
+		int avg_efficiency = 0;
+		int count = 0;
+		int countMonth = 0;
+		int avgEfficiency = 0;
+		int month = 0;
+		int year = 0;
+		HashMap<Integer,Integer> EfficiencyHashMap=new HashMap<Integer,Integer>();//Creating HashMap    
+		LocalDate startDate = null;
+		LocalDate endDate = null;
+		LocalDate closeDate = null;
+		long durationTaken = 0;
+		long durationGiven = 0;
+		 
+		while(resultSetEfficiency.next()) 
+    	{
+    		//System.out.println("\nStart Date:" +resultSet.getString("startDate"));
+    		//System.out.println("\nEnd Date:" +resultSet.getString("endDate"));
+    		//System.out.println("\nClose Date:" +resultSet.getString("closeDate"));
+    		
+    	    startDate = LocalDate.parse(resultSetEfficiency.getString("startDate"));
+    	    endDate = LocalDate.parse(resultSetEfficiency.getString("endDate"));
+    	    durationGiven = ChronoUnit.DAYS.between(startDate,endDate);
+    	    //System.out.print("\nDifference duration Given:" +durationGiven);
+
+    	    closeDate = LocalDate.parse(resultSetEfficiency.getString("closeDate"));
+    	    durationTaken = ChronoUnit.DAYS.between(startDate,closeDate);
+    	    
+    	    //System.out.print("\nDifference duration taken:" +durationTaken);
+    	    durationTaken++;
+    	    durationGiven++;
+    	    
+    	    
+	    	if(durationTaken < ((int) durationGiven/2)) {
+    	    	if(durationGiven>10) {
+    	    		myVar = WorkWeights.HIGH;
+    	    	}
+    	    	
+    	    	else if(durationGiven>5 && durationGiven<11) {
+    	    		myVar =  WorkWeights.MEDIUM; 
+    	    	}
+    	    	else {
+    	    		myVar = WorkWeights.LOW; 
+    	    	}
+	    		efficiency= (int) ((durationTaken*100*myVar.getWorkWeights())/durationGiven);
+    	    	//System.out.print("\nEfficiency:" +efficiency);
+	    	}
+	    	else 
+	    	{
+	    		efficiency= (int) ((durationTaken*100)/durationGiven);
+	    		//System.out.print("\nEfficiency:" +efficiency);
+	    	}
+	    	avg_efficiency +=efficiency;
+	    	count++;
+	    	
+	    	
+	    	if(startDate.getMonthValue() == month || count==1)
+	    	{
+	    	    avgEfficiency +=efficiency;
+		    	countMonth++;
+	    	}
+	    	else
+    	    {
+    	    	avgEfficiency = avgEfficiency/countMonth;	
+     	    	EfficiencyHashMap.put(month, avgEfficiency);
+    			avgEfficiency = 0;
+    			countMonth = 0;
+	    		avgEfficiency += efficiency;
+	    		countMonth++;
+    	    }
+	    	month = startDate.getMonthValue();
+    	}
+		
+		year = startDate.getYear();
+		avg_efficiency = avg_efficiency/count;
+		EfficiencyHashMap.put(year, avg_efficiency);
+		
+		EfficiencyHashMap.put(month, avgEfficiency);
+		
+		System.out.print(EfficiencyHashMap);
+		
+		return EfficiencyHashMap;
+	}
+	
 	
 }
