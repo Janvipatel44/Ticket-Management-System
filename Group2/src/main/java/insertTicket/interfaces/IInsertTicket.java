2fea88571f2b552c07158c29115895170943d1e3
janvi <jn410076@dal.ca>
2021-04-03 05:44:48 -0300

    removed validation method to follow single responsibility

--- Group2/src/main/java/insertTicket/interfaces/IInsertTicket.java
+++ Group2/src/main/java/insertTicket/interfaces/IInsertTicket.java
@@ -1,11 +1,10 @@
 package insertTicket.Interfaces;
 
 import java.text.ParseException;
-import java.util.HashMap;
 
 public interface IInsertTicket {
 	
-	public HashMap userInputTicketDetails() throws ParseException;
-	public boolean validateInsertion() throws ParseException;
+	public boolean userInputTicketDetails() throws ParseException;
+	//public boolean validateInsertion() throws ParseException;
 	public void successfulInsertion() throws ParseException;
 }
