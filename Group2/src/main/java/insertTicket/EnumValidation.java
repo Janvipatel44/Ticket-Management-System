3450b19a018ae0f5b61cf7da00b18aa47b4d2083
janvi <jn410076@dal.ca>
2021-04-03 05:43:32 -0300

    Enum used as switch case for input validation

--- Group2/src/main/java/insertTicket/EnumValidation.java
+++ Group2/src/main/java/insertTicket/EnumValidation.java
@@ -5,9 +5,7 @@ public enum EnumValidation {
 	
 		validateTicketID,	//null & special character, length
 	    validateDescription,	//null & special character
-	    validateStartDate,		//automatically generated
 	    validateExpectedEndDate,	//	range
-	    validateEndDate,			//range	    
 	    validateReporterID,			//employeeReporterPrefix , range, special character
 	    validateEmployeeID,			//employeeReporterPrefix , range, special character
 	    validateAssigneeName,		//range, special character
@@ -20,10 +18,6 @@ public enum EnumValidation {
 	    validateCustomerName,		//special character
 	    validateTicketLevel,		//not null
 	    validateCreatorID,			//not null, special character, range
-	    validateCreatorName,		//not null, special character, range
-	    validateRating,				//range
-	    validateAttachmentID,		
-	    validateWorkingHours,		//non negative
-	    validateTicketOnHoldHours,	//non negative 
-	    validateTicketProgressHours	//non negative
+	    validateCreatorName		//not null, special character, range
+	    
 }
