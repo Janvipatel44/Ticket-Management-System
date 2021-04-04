15c64f7f2e129efa5eb14ac031a99f68ee24395e
janvi <jn410076@dal.ca>
2021-04-04 11:21:17 -0300

    deleted interfaces

--- Group2/src/main/java/insertTicket/interfaces/IInputDateValidation.java
+++
@@ -1,9 +0,0 @@
-package insertTicket.Interfaces;
-
-import java.text.ParseException;
-
-public interface IInputDateValidation 
-{
-		public boolean isDurationValid (String startDate, String endDate) throws ParseException;
-		public boolean isDateFormatValid (String validationString) throws ParseException;
-}
