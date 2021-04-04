6bb58a624810b468b3b3e9636232320d5d739bba
Vamsi Krishna Utla <vm271757@dal.ca>
2021-04-03 21:21:15 -0300

    Deleted the abstract classes in Fcatory of Login

--- Group2/src/main/java/database/abstractfactory/IDatabaseFactory.java
+++ Group2/src/main/java/database/abstractfactory/IDatabaseFactory.java
@@ -1,14 +1,9 @@
 package database.abstractfactory;
-
 import Tickets.ICreateTicket;
-import database.IConnectionManager;
-import database.IDatabaseOperations;
-import database.ITicketOperationsDB;
-
-public interface IDatabaseFactory {
+import database.*;
+public interface IDatabaseFactory
+{
     IConnectionManager getConnectionManager(String configurationFile);
-
     IDatabaseOperations getDatabaseOperations();
-
     ITicketOperationsDB getTicketOperationsDB(ICreateTicket createTicket);
 }
