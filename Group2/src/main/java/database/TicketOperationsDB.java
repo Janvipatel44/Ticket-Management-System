eee6b3c101c2e3fcbda5d4a461f985a302dde8c0
janvi <jn410076@dal.ca>
2021-04-04 18:32:28 -0300

    feature generate Ticket

--- Group2/src/main/java/database/TicketOperationsDB.java
+++ Group2/src/main/java/database/TicketOperationsDB.java
@@ -2,10 +2,12 @@ package database;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.HashMap;
+
+import insertTicket.Interfaces.ICreateTicket;
 
 import java.sql.CallableStatement;
-
-import Tickets.ICreateTicket;
 
 public class TicketOperationsDB implements ITicketOperationsDB{
 
@@ -14,28 +16,36 @@ public class TicketOperationsDB implements ITicketOperationsDB{
  
 	IConnectionManager IConnectionMng = new ConnectionManager(ConfigurationFile);
 	ICreateTicket createTicket = null;
+    HashMap<String, String> inputsHandler = new HashMap<String, String>();
+
 	public TicketOperationsDB(ICreateTicket createTicket) {
 		this.createTicket = createTicket;
 	}
-	public boolean insertTicket()
+	public boolean insertTicket() throws ParseException
 	{
 		connection = IConnectionMng.establishConnection();
         boolean success=false;
 		try {
-			CallableStatement statement = (CallableStatement) connection.prepareCall("{call insertTicket(?, ?, ?,?,?,?,?,?,?,?,?,?)}");
+			System.out.print("Insertion");
+			CallableStatement statement = (CallableStatement) connection.prepareCall("{call insertTicket(?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?)}");
+		
 			
-			statement.setString(1, createTicket.getTicketId());
+			statement.setString(1, createTicket.getTicketID());
             statement.setString(2, createTicket.getDescription() );
-            statement.setDate(3, java.sql.Date.valueOf(createTicket.getStartDate()) );
-            statement.setDate(4, java.sql.Date.valueOf(createTicket.getEndDate()) );
-            statement.setString(5, createTicket.getReporterId() );
-            statement.setString(6, createTicket.getEmployeeId() );
-            statement.setString(7, createTicket.getAssigneeName() );
+            statement.setTimestamp(3, new java.sql.Timestamp(new java.util.Date().getTime()));
+            statement.setTimestamp(4, new java.sql.Timestamp( createTicket.getExpectedEndDate().getTime()));
+            statement.setString(5, createTicket.getReporterID());
+            statement.setString(6, createTicket.getEmployeeID());
+            statement.setString(7, createTicket.getAssigneeName());
             statement.setString(8, createTicket.getTicketType() );
             statement.setInt(9, createTicket.getPriority() );
-            statement.setInt(10, createTicket.getUrgency() );
+            statement.setInt(10, createTicket.getUrgency());
             statement.setInt(11, createTicket.getImpact() );
-            statement.setString(12, createTicket.getComments() );
+            statement.setString(12, createTicket.getTicketLevel());
+            statement.setString(13, createTicket.getCustomerID());
+            statement.setString(14, createTicket.getCustomerName());
+            statement.setString(15, createTicket.getCreatorID());
+            statement.setString(16, createTicket.getCreatorName());
 
 	        statement.execute();
 	        success = true;
@@ -47,30 +57,17 @@ public class TicketOperationsDB implements ITicketOperationsDB{
         return success;
 	}	
 	
-	public boolean duplicateTicket() {
+	public boolean duplicateTicket() throws ParseException {
 		
 		connection = IConnectionMng.establishConnection();
 		int duplicate_ticket = 0;
         boolean success=false;
 		try {
-			CallableStatement statement = (CallableStatement) connection.prepareCall("{call duplicateTicket(?, ?, ?,?,?,?,?,?,?,?,?,?,?)}");
+			CallableStatement statement = (CallableStatement) connection.prepareCall("{call duplicateTicket(?,?)}");
 
-			statement.setString(1, createTicket.getTicketId());
-            statement.setString(2, createTicket.getDescription() );
-            statement.setDate(3, java.sql.Date.valueOf(createTicket.getStartDate()) );
-            statement.setDate(4, java.sql.Date.valueOf(createTicket.getEndDate()) );
-            statement.setString(5, createTicket.getReporterId() );
-            statement.setString(6, createTicket.getEmployeeId() );
-            statement.setString(7, createTicket.getAssigneeName() );
-            statement.setString(8, createTicket.getTicketType() );
-            statement.setInt(9, createTicket.getPriority() );
-            statement.setInt(10, createTicket.getUrgency() );
-            statement.setInt(11, createTicket.getImpact() );
-            statement.setString(12, createTicket.getComments() );
-            statement.setInt(13, java.sql.Types.INTEGER);  
-
+			statement.setString(1, createTicket.getTicketID());
             statement.execute();
-            duplicate_ticket = statement.getInt(13);
+            duplicate_ticket = statement.getInt(2);
             System.out.print("duplicate_ticket:" +duplicate_ticket);
 			if(duplicate_ticket==0) {
 			    success = false;
