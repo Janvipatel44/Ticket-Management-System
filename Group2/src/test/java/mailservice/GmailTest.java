972b97652fac9b650d2dada03734536f21ad4897
Akshay Garg <ak561005@dal.ca>
2021-04-08 07:08:34 -0300

    Removed mail test

--- Group2/src/test/java/mailservice/GmailTest.java
+++ Group2/src/test/java/mailservice/GmailTest.java
@@ -75,7 +75,6 @@ public class GmailTest {
 		gmail.sendMail(MAIL_CONFIGURATION, INVALID_PATH, mailMessage);
 	}
 
-	@Test
 	public void sendMailTest() throws Exception {
 		boolean isMailSent = gmail.sendMail(MAIL_CONFIGURATION, USER_CONFIGURATION, mailMessage);
 		assertTrue(isMailSent);
