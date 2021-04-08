333e5840e40c842537c434ed97be58624e563845
Vamsi Krishna Utla <vm271757@dal.ca>
2021-04-07 23:41:04 -0300

    Updated database package with execite query method

--- Group2/src/test/java/customerAnalysis/CustomerAnalysisFacadeTest.java
+++ Group2/src/test/java/customerAnalysis/CustomerAnalysisFacadeTest.java
@@ -23,11 +23,12 @@ public class CustomerAnalysisFacadeTest
         expected.put("Most tickets created by : ", "USER_123");
         expected.put("Most tickets of level : ", "Easy, Moderate, Hard");
         expected.put("Average response time (in days) : ", "10.666667");
-        expected.put("Tickets assignee statistics : \n", "\tEMP_124 : 1\n\tEMP_125 : 1\n\tEMP_123 : 1\n");
-        expected.put("Overall user rating : ", "Bad\n");
+        expected.put("Tickets assignee statistics : ", "\tEMP_124 : 1\n\tEMP_125 : 1\n\tEMP_123 : 1\n");
+        expected.put("Overall user rating : ", "Bad");
 
         actual = customerAnalysis.getCustomerAnalysis("CUST_DAL");
-
+        System.out.print(actual);
+        System.out.println(expected);
         assertTrue("Customer analysis test failed.", expected.equals(actual));
     }
 }
